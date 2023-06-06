39dc9c778aced0aa3a059324203a790bc4be9f12
QIZHUO HUANG <qizhuohuang@fortna.com>
2023-06-06 18:27:05 -0400

    efed

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 import './css/style.css'
+import FullList from './model/FullList'
+import ListTemplates from './templates/ListTemplates';
+
+const initApp = ():void => {
+   const fullList = FullList.instance;
+   const listTemplates = ListTemplates.instance;
+   listTemplates.render(fullList);
+}
+
+document.addEventListener("DOMContentLoaded", initApp)
--- src/templates/ListTemplates.ts
+++ src/templates/ListTemplates.ts
@@ -5,3 +5,53 @@ interface DOMList {
     clear(): void,
     render(fullList: FullList): void,
 }
+
+export default class ListTemplates implements DOMList{
+    ul: HTMLUListElement;
+
+    static instance: ListTemplates = new ListTemplates();
+
+    constructor() {
+        this.ul = document.getElementById("listItems") as HTMLUListElement;
+    }
+
+    clear() {
+       this.ul.innerHTML = ''
+    }
+
+    render(fullList: FullList): void {
+        fullList.list.forEach(item => {
+            const li = document.createElement("li") as HTMLLIElement;
+            li.className = "item";
+            
+            const input = document.createElement("input") as HTMLInputElement;
+            input.type = "checkbox";
+            input.id = item.id;
+            input.checked = item.checked;
+            li.append(input);
+
+            input.addEventListener('change', () => {
+                item.checked = !item.checked;
+                fullList.save();
+            })
+            
+            const label = document.createElement("label") as HTMLLabelElement;
+            label.htmlFor = item.id;
+            label.textContent = item.item;
+            li.append(label);
+
+            const button = document.createElement("button") as HTMLButtonElement;
+            button.className = "button";
+            button.textContent = 'X';
+            li.append(button);
+            
+            button.addEventListener('click', () => {
+                fullList.removeItem(item.id);
+                this.render(fullList);
+            })
+
+            this.ul.append(li);
+        })
+    }
+
+}
